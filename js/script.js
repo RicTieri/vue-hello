54cb4b974b34578add97868b7969b449be869580
RicTieri <tieri.riccardo@gmail.com>
2023-11-30 17:04:54 +0100

    added dom output via vuejs

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            title: 'Planetary Vue',
+            image_src: './img/earth-space-black-background-atmosphere-blue-planet-5k-8k-12016x12016-2152.jpg',
+            caption: 'view of Earth from the universe'
         }
     },
 }).mount('#app');
